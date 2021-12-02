9ffc74b0aab10bf4a10c93d360caf33fbf5029eb
Rstar284 <supermanrehan284@gmail.com>
2021-12-02 12:45:16 +0400

    day1_part1

---
+++ day1/main.py
@@ -0,0 +1,9 @@
+with open('./input.txt', 'r') as f:
+    input = list(map(int, f.read().splitlines()))
+
+# Part 1
+a = 0
+for i in range(1, len(input)):
+    if input[i] > input[i-1]:
+        a = a + 1
+        print(a)
