b9a07d6a961cb79b7783d8e0a9a4a0f4d8ae7a4a
Rstar284 <supermanrehan284@gmail.com>
2021-12-02 13:55:26 +0400

    day2_part1

---
+++ day2/main.py
@@ -0,0 +1,22 @@
+with open('input.txt', 'r') as f:
+    input = f.read().splitlines()
+
+x = y = depth = depth1 = 0
+
+for line in input:
+    cmd,num = line.split()
+    num = int(num)
+    if cmd == 'forward':
+        y += num
+        depth1 += x * num
+    if cmd == 'up' :
+        x -= num
+        depth -= num
+    if cmd == 'down':
+        x += num
+        depth += num
+
+print(y * depth)
+
+
+
