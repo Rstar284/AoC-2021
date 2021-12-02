2bdab628fd8d97fc814b5f12cec66ed4887ac170
Rstar284 <supermanrehan284@gmail.com>
2021-12-02 13:08:11 +0400

    day1_part2

--- day1/main.py
+++ day1/main.py
@@ -6,5 +6,13 @@ a = 0
 for i in range(1, len(input)):
     if input[i] > input[i-1]:
         a = a + 1
-
 print(a)
+
+# Part 2
+b = 0
+for j in range(0, len(input)-1):
+    A = input[j-2] + input[j-1] + input[j]
+    B = input[j-1] + input[j+1] + input[j]
+    if A < B:
+        b = b + 1
+print(b)
